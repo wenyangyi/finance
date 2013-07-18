9c6316f23d78055439f33764ac3a08bc13e2dbf7
wenyangyi <wenyangyi74@gmail.com>
2013-07-18 18:03:14 +0900

    finance_git test

---
+++ finance/src/com/wen/finance/SignServlet.java
@@ -0,0 +1,34 @@
+package com.wen.finance;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+
+public class SignServlet extends HttpServlet {
+	private static final Logger log = Logger.getLogger(SignServlet.class.getName());
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+                throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        String content = req.getParameter("content");
+        if (content == null) {
+            content = "(No greeting)";
+        }
+        if (user != null) {
+            log.info("Greeting posted by user " + user.getNickname() + ": " + content);
+        } else {
+            log.info("Greeting posted anonymously: " + content);
+        }
+        resp.sendRedirect("/view_result.jsp");
+    }
+}
